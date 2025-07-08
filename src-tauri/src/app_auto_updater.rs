bc1b9e9757030acaf4213f6afa9c593bc1509745
zhom <2717306+zhom@users.noreply.github.com>
2025-07-08 17:36:58 +0400

    style: copy

--- src-tauri/src/app_auto_updater.rs
+++ src-tauri/src/app_auto_updater.rs
@@ -455,7 +455,7 @@ impl AppAutoUpdater {
             percentage: Some(percentage),
             speed: Some(format!("{speed:.1}")),
             eta: Some(eta),
-            message: format!("Downloading update..."),
+            message: "Downloading update...".to_string(),
           },
         );
 
